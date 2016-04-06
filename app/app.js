81cd989e84780c0188e97b394417350e8ece5003
arielcessario <arielcessario@gmail.com>
2016-04-06 10:57:29 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -11,7 +11,35 @@
         'auth0',
         'acUtils'
     ]).config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/view1'});
-    }]);
+            $routeProvider.otherwise({redirectTo: '/view1'});
+        }])
+        .controller('AppCtrl', AppCtrl);
+
+    AppCtrl.$inject = ['$http'];
+    function AppCtrl($http) {
+        var vm = this;
+        vm.get = function () {
+            $http.get('usuarios.php?function=get')
+                .success(function (data) {
+                        console.log(data);
+                    }
+                )
+                .error(function (data) {
+                        console.log(data);
+                    }
+                );
+        };
+        vm.post = function () {
+            $http.post('usuarios.php',{'function':'post', 'param':'prueba'})
+                .success(function (data) {
+                        console.log(data);
+                    }
+                )
+                .error(function (data) {
+                        console.log(data);
+                    }
+                );
+        }
+    }
 
 })();
---
+++ app/includes/utils.php
@@ -0,0 +1,122 @@
+<?php
+
+// JWT Secret Key
+//$secret = base64_encode('asdfwearsadfasdareasdfaeasdfaefawasadf');
+$secret = 'asdfwearsadfasdareasdfaeasdfaefawasadf';
+// JWT Secret Key Social
+$secret_social = 'LUc_cGQHgmKZyFd5ozKJHnujpam1JKb06FWnjjtnWH9htNKDEQFGNMHYUvX_6PgR';
+// JWT AUD
+$serverName = 'serverName';
+// false local / true production
+$jwt_enabled = true;
+// Carpeta de imágenes
+$image_path = "../../images/";
+// MyDBi
+require_once 'MyDBi.php';
+
+/* @name: checkSecurity
+ * @param
+ * @description: Verifica las credenciales enviadas. En caso de no ser correctas, retorna el error correspondiente.
+ */
+function checkSecurity()
+{
+    $requestHeaders = apache_request_headers();
+    $authorizationHeader = $requestHeaders['Authorization'];
+//    echo print_r(apache_request_headers());
+
+
+    if ($authorizationHeader == null) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "No authorization header sent";
+        exit();
+    }
+
+    // // validate the token
+    $pre_token = str_replace('Bearer ', '', $authorizationHeader);
+    $token = str_replace('"', '', $pre_token);
+    global $secret;
+    global $decoded_token;
+    try {
+//        $decoded_token = JWT::decode($token, base64_decode(strtr($secret, '-_', '+/')), true);
+        $decoded_token = JWT::decode($token, $secret, true);
+    } catch (UnexpectedValueException $ex) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "Invalid token";
+        exit();
+    }
+
+
+    global $serverName;
+
+    // // validate that this token was made for us
+    if ($decoded_token->aud != $serverName) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "Invalid token";
+        exit();
+    }
+
+}
+
+/**
+ * @description Valida que el rol del usuario sea el correcto
+ * @param $requerido
+ */
+function validateRol($requerido)
+{
+
+    global $jwt_enabled;
+    if ($jwt_enabled == false) {
+        return;
+    }
+
+    $requestHeaders = apache_request_headers();
+
+    $authorizationHeader = $requestHeaders['Authorization'];
+//    echo print_r(apache_request_headers());
+
+
+    if ($authorizationHeader == null) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "No authorization header sent";
+        exit();
+    }
+
+    // // validate the token
+    $pre_token = str_replace('Bearer ', '', $authorizationHeader);
+    $token = str_replace('"', '', $pre_token);
+    global $secret;
+    global $decoded_token;
+    $decoded_token = JWT::decode($token, $secret, true);
+
+    $rol = $decoded_token->data->rol;
+    if ($rol > $requerido) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "No authorization header sent";
+        exit();
+    }
+
+
+}
+
+
+class Main
+{
+    public static $db;
+    private $permisssions = array(
+        'Usuarios' => array('get' => 1, 'po' => 0)
+
+    );
+
+
+    protected function __construct($class, $fnc)
+    {
+        try {
+            validateRol($this->permisssions[$class][$fnc]);
+        } catch (Exception $e) {
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+        }
+        if(!isset($this->db)){
+            $this->db = new MysqliDb();
+        }
+    }
+}
