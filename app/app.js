1d4f0e887f02943acdd76ecebff9cb607a66195c
arielcessario <arielcessario@gmail.com>
2016-04-12 19:22:36 -0300

    Ariel

--- app/app.js
+++ app/app.js
@@ -11,8 +11,24 @@
         'auth0',
         'acUtils',
         'acAutocomplete'
-    ]).config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.otherwise({redirectTo: '/view1'});
+    ]).config(['$routeProvider', 'authProvider', function ($routeProvider, authProvider) {
+            authProvider.init({
+                domain: 'ac-desarrollos.auth0.com',
+                clientID: 'su5JUmdUk52EWhfK5xxZJtnw6W3IK9c1',
+                loginUrl: '/ingreso'
+            });
+
+        $routeProvider.when('/main', {
+                templateUrl: 'main/main.html',
+                controller: 'MainController',
+                data: {requiresLogin: false},
+                resolve: { // Any property in resolve should return a promise and is executed before the view is loaded
+                    loadMyCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+                        // you can lazy load files for an existing module
+                        return $ocLazyLoad.load('main/main.js');
+                    }]
+                }
+            });
         }])
         .controller('AppCtrl', AppCtrl);
 
